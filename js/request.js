5b59c3448327ab1641c696f6544d60b21b3d96e4
newive <738767136@qq.com>
2020-10-14 19:21:07 +0800

    'finish-google-parse-module'

--- js/request.js
+++ js/request.js
@@ -4,16 +4,20 @@ window.str = '';
 // TODO: handle youdao translate page
 
 async function reqBaidu() {
+    // food
+    // red a
+    // kksk
     let q = window.str;
     try {
-        return await window.browser.goto(`https://fanyi.baidu.com/#en/zh/${genQuery(q)}`)
+        let d = await window.browser.goto(`https://fanyi.baidu.com/#en/zh/${genQuery(q)}`)
             .wait('.target-output')
             .evaluate(() => {
                 let title = document.getElementsByTagName('textarea')[0].value;
                 let spell = document.getElementsByClassName('target-output')[0].getElementsByTagName('span')[0].innerText;
                 return {
                     title,
-                    spell
+                    spell,
+                    msg: 1
                 }
             })
             .when('.simple-dict')
@@ -38,7 +42,7 @@ async function reqBaidu() {
                     let comment = handleComments();
                     return {
                         msg: 1,
-                        comments: comment,
+                        translations: comment,
                         type: 'common'
                     };
                 }
@@ -73,17 +77,111 @@ async function reqBaidu() {
             .run({
                 show: false
             });
+        return {
+            data: d,
+            from: 'baidu'
+        }
     } catch (e) {
         return {
             msg: 0,
             err: e,
-            tips: 'parsing content error'
+            tips: 'parsing content error',
+            from: 'baidu'
         };
     }
 }
 
 async function reqGoogle() {
+    let q = window.str;
+    try {
+        return await window.browser.goto(`https://translate.google.cn/#view=home&op=translate&sl=en&tl=zh-CN&text=${genQuery(q)}`)
+            .wait('.tlid-translation')
+            .evaluate(() => {
+                let title = document.getElementById('source').value;
+                let spell = document.getElementsByClassName('tlid-translation')[0].getElementsByTagName('span')[0].innerText;
+                return {
+                    title,
+                    spell,
+                    msg: 1
+                }
+            })
+            .when('.gt-baf-table')
+            .evaluate(() => {
+                let list = new Array(...document.getElementsByClassName('gt-baf-table')[0].getElementsByTagName('tr'));
+                let index = -1;
+                let tempList = [];
+                let o = [];
+                list.forEach((i) => {
+                    console.log(i);
+                    if(/gt-baf-entry/.test(i.className)) {
+                        let t = i.getElementsByTagName('td');
+                        o[index].list.push({
+                            spell: t[0].getElementsByClassName('gt-baf-cell')[0].innerText,
+                            similar: new Array(...t[1].getElementsByClassName('gt-baf-back')).map((i) => (
+                                i.innerText
+                            ))
+                        })
+                    } else {
+                        tempList.push(i.getElementsByClassName('gt-cd-pos')[0].innerText);
+                        o.push({
+                            type: tempList[++index],
+                            list: []
+                        });
+                    }
+                });
+                return {
+                    translations: o
+                }
+            })
+            .end()
+            .when('.gt-def-list')
+            .evaluate(() => {
+                let tempList = [];
+                let o = [];
+                let index = -1;
+                new Array(
+                    ...document.getElementsByClassName('gt-cd-mmd')[0]
+                    .getElementsByClassName('gt-cd-c')[0]
+                    .children
+                )
+                    .forEach((i) => {
+                        console.log(i);
+                        if(/gt-cd-pos/.test(i.className)) {
+                            tempList.push(i.innerText);
+                            o.push({
+                                type: tempList[++index],
+                                definitions: []
+                            });
+                        } else if(/gt-def-list/.test(i.className)) {
+                            new Array(...i.getElementsByClassName('gt-def-info')).forEach((item) => {
+                                let objTemp = {
+                                    def: item.getElementsByClassName('gt-def-row')[0].innerText
+                                }
+                                try {
+                                    objTemp['example'] = item.getElementsByClassName('gt-def-example')[0].getElementsByTagName('q')[0].innerText
+                                } catch (e) {
 
+                                }
+                                 o[index].definitions.push(objTemp);
+                            });
+                        }
+                    });
+                return {
+                    definitions: o
+                }
+            })
+            .end()
+            .run({
+                show: false
+            })
+    } catch (e) {
+        return {
+            msg: 0,
+            err: e,
+            tips: 'parsing content error',
+            from: 'google'
+        };
+    }
 }
 
 async function reqYD() {
@@ -93,6 +191,6 @@ async function reqYD() {
 // test module
 (function init() {
     document.getElementById('nibaba').onclick = async () => {
-        console.log(await reqBaidu());
+        console.log(await reqGoogle());
     };
 })()
